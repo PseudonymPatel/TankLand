827544478f527b17a723d3286e8a576cfe7eaed3
Sheen Patel <programmingpatel@gmail.com>
2019-04-22 21:21:15 -0400

    created TankWorld class, brought up to first day spec

---
+++ TankWorld.swift
@@ -0,0 +1,45 @@
+/*
+*	This file contains only the TankWorld class. 
+*
+*
+*/
+
+class TankWorld {
+	
+	// -------------------------------------
+	// Properties
+	// -------------------------------------
+	
+	//an instance of Grid.
+	let grid:Grid = Grid()
+
+
+	// -------------------------------------
+	// Initializer
+	// -------------------------------------
+	init() {
+		//does nothing, yet!
+	}
+
+
+	// -------------------------------------
+	// Methods
+	// -------------------------------------
+
+	//adds an object to the grid.
+	//uses the position stored in the object as the inital position.
+	//assumes that all positions are valid
+	func addObject(_ object:GameObject) {
+		grid.addObject(object)
+	}
+
+	//moves object to a Position
+	func moveObject(_ object:GameObject, toPosition:Position) {
+		grid.moveObject(object, toRow:toPosition.row, toCol:toPosition.col)
+	}
+
+	//exactly what it sounds like
+	func displayGrid() {
+		grid.displayGrid()
+	}
+}
