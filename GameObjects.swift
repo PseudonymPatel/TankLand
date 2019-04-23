5879e60a88e261c530e6a26bf2ee1252cdd779f2
Sheen Patel <programmingpatel@gmail.com>
2019-04-22 21:25:41 -0400

    brought this up to first day spec (#2) all files!!
    * brought this up to first day spec
    
    * used the example provided for first day spec
    
    * created TankWorld class, brought up to first day spec
    
    * brought up to first day spec + movement

--- GameObjects.swift
+++ GameObjects.swift
@@ -3,13 +3,13 @@
 *	Tanks
 *	Mines
 *	Rovers
-*
+*	*Position Struct*
 *
 */
 
 //The class GameObject should be a superclass to all the possible objects on the TankLand grid
 //Will house basic funcion declarations for polymorphism.
-class GameObject {
+class GameObject:CustomStringConvertible {
 	
 	// ---------------------------------------------------------
 	// Properties
@@ -17,19 +17,28 @@ class GameObject {
 
 	//Health/Energy of the GameObject
 	var energy:Int
+	//name that will be shown on grid
 	let name:String
+	//to keep track of the position of the GameObject
+	var position:Position
+
+	var description:String {
+		return "\(self.name) at position \(position)"
+	}
 
 	// ---------------------------------------------------------
 	// Initalizers
 	// ---------------------------------------------------------
-	init(energy:Int, name:String) {
+	init(name:String, energy:Int, position:Position) {
 		self.energy = energy
 		self.name = name
+		self.position = position
 	}
 
-	init(name:String) {
+	init(name:String, position:Position) {
 		self.energy = 10000
 		self.name = name
+		self.position = position
 	}
 
 	// ---------------------------------------------------------
@@ -38,4 +47,27 @@ class GameObject {
 	func hurt(_ energy:Int) {
 		self.energy -= energy
 	}
+}
+
+struct Position:ExpressibleByArrayLiteral, CustomStringConvertible {
+
+	//Properties
+	var row:Int
+	var col:Int
+
+	var description:String {
+		return "(\(self.row), \(self.col))"
+	}
+
+	//Initalizers
+	init(row:Int, col:Int) {
+		self.row = row
+		self.col = col
+	}
+
+	init(arrayLiteral: Int...) { //convinient init so i can do: Position(1, 3) or let pos:Position = [1, 3]
+		assert(arrayLiteral.count == 2, "Must initialize Position with 2 values only!")
+		self.row = arrayLiteral[0]
+		self.col = arrayLiteral[1]
+  	}
 }
