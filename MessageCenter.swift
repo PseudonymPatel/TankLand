abe7176c8c3b794e0c7b558b6c74ed2113e4f636
meppydc <49413486+meppydc@users.noreply.github.com>
2019-04-29 15:24:00 -0400

    initial commit

---
+++ MessageCenter.swift
@@ -0,0 +1,47 @@
+/*
+*	This file is for everything having to do with the organization of messages.
+*	Included here is the code for message storage and all classes and helper functions related.
+*
+*	** MessageCenter Struct **
+*	
+*/
+struct MessageCenter {
+
+	// ---------------------------------------------------------
+	// Properties
+	// ---------------------------------------------------------
+
+	//contains all the messages 
+	private var messages:[String:String]
+
+
+	// ---------------------------------------------------------
+	// Initializers
+	// ---------------------------------------------------------
+
+	init() {
+		messages = [:]
+	}
+
+	// ---------------------------------------------------------
+	// Methods
+	// ---------------------------------------------------------
+
+	//Helper function to add a message
+	//@param id the identification of the message to store
+	//@param message the message
+	mutating func addMessage( id: String, message: String) {
+		messages[id] = message
+	}
+
+	//Helper function to retrieve message
+	//@param id the identification of the message to retrieve
+	//@return message if exists or a default message
+	func retrieveMessage( id: String) -> String{
+		if let message = messages[id] {
+			return message
+		} else {
+			return "The message with id \(id) doesn't exist."
+		}
+	}
+}
