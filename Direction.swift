efcd481ab0d9bb7260e1a957767709eab4130ad0
Sheen Patel <programmingpatel@gmail.com>
2019-04-30 12:32:24 +0000

    All the lowlevel stuff complete except GO

---
+++ Direction.swift
@@ -0,0 +1,8 @@
+/*
+*	This will be just for Direction.
+*	Used in the movement function, where the direction is specified and the function returns the Position
+*/
+
+enum Direction {
+	case North, South, East, West, NorthEast, NorthWest, SouthEast, SouthWest
+}
