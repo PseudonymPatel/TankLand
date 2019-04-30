4201ccbfdb1c9914b67217baabe4b1efa757d74a
Sheen Patel <programmingpatel@gmail.com>
2019-04-30 15:49:25 +0000

    Finished up with basics

--- main.swift
+++ main.swift
@@ -1,22 +1,4 @@
-print("If the last line is not: \"End of program\", something has gone wrong with repl.it, refresh page and rerun a couple times")
+print("Testing for TankWorld")
+let tankWorld = TankWorld()
+tankWorld.displayGrid()
 
-//following example handout!
-let testingObject = GameObject(name:"J1", position:[5,6])
-let obj2 = GameObject(name:"J2", energy:9000, position:[6,6])
-let obj3 = GameObject(name:"J3", energy:8000, position:[7,5])
-let obj4 = GameObject(name:"J4", energy:7000, position:[10,3]) //oh no wrong position. move it later!!
-let tankLand = TankWorld()
-
-tankLand.addGameObject(testingObject)
-tankLand.addGameObject(obj2)
-tankLand.addGameObject(obj3)
-tankLand.addGameObject(obj4)
-tankLand.displayGrid() //J4 is at wrong pos!
-
-print("Oh no, J4 is wrong, moving it!!\n")
-
-tankLand.moveObject(obj4, toRow:12, toCol:3)
-tankLand.displayGrid() //now display the correct one!
-
-print("\n")
-print("End of program")
