a4454c99c95d3a04b0b2bf6133d389dccd39eae9
meppydc <49413486+meppydc@users.noreply.github.com>
2019-05-01 10:02:46 -0400

    cut Position struct

--- GameObjects.swift
+++ GameObjects.swift
@@ -1,7 +1,7 @@
 /*
 *	This file will house the GameObject class and :
 *	GameObjectType
-*	*Position Struct*
+*	
 *
 */
 
@@ -63,27 +63,4 @@ class GameObject:CustomStringConvertible {
 		position = newPosition
 	}
 
-}
-
-struct Position:ExpressibleByArrayLiteral, CustomStringConvertible {
-
-	//Properties
-	var row:Int
-	var col:Int
-
-	var description:String {
-		return "(\(self.row), \(self.col))"
-	}
-
-	//Initalizers
-	init(row:Int, col:Int) {
-		self.row = row
-		self.col = col
-	}
-
-	init(arrayLiteral: Int...) { //convinient init so i can do: Position(1, 3) or let pos:Position = [1, 3]
-		assert(arrayLiteral.count == 2, "Must initialize Position with 2 values only!")
-		self.row = arrayLiteral[0]
-		self.col = arrayLiteral[1]
-  	}
 }
