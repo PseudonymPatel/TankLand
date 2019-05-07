d257597ca17412c606fc9d742dd30c693ea17870
meppydc <49413486+meppydc@users.noreply.github.com>
2019-05-07 08:26:12 -0400

    initial commit
    🤔 💭

---
+++ Actions/SendMessageAction.swift
@@ -0,0 +1,15 @@
+//SendMessageAction Class
+
+class SendMessageAction: PostAction {
+	let action: Actions
+	let id: String
+	let message: String
+
+	init(id: String, message: String) {
+		self.action = .SendMessageAction
+	}
+
+	var description: String {
+		return \(action) \(id) \(direction)
+	}
+}
